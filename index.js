028f53818160b8b49bed34bf304e5f77f2602e89
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-27 21:15:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = {}
+  
+  for (var i = 0; i < musicians.length; i++){
+    var temp = `${musicians[i]} plays ${instruments[i]}`
+    array.push(temp)
+  }
+  return array 
+}
+
+function johnLennonFacts(array){
+  var result = {}
+  for (var i = 0; i < array.length; i++){
+    var temp = array[i] + "!!!"
+    result.push(temp)
+  }
+}
+
+function iLoveTheBeatles(n){
+  var result = {}
+  
+}
