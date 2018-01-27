5bfbe57bb02716557e540d6320a83a7db068ee61
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-27 21:16:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ function johnLennonFacts(array){
 
 function iLoveTheBeatles(n){
   var result = {}
-  
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while(n < 15)
+  return result
 }
